6b1c462009492b5127b4d0c5375b7ff9076a4cb0
jrpark <jrpark@gmail.com>
2024-06-10 01:34:03 +0000

    Merge 240610

--- test.py
+++ test.py
@@ -1,35 +1,5 @@
 import mysql.connector as mariadb
 
-<<<<<<< HEAD
-my_cnx = mariadb.connect(
-    host='172.17.0.2',
-    port=3306,
-    user='myuser',
-    password='mypassword',
-    database='test',
-)
-
-my_cursor = my_cnx.cursor()
-
-my_cursor.execute("""
-    CREATE TABLE TB_TZ_MST (
-        ID INT(8) NOT NULL AUTO_INCREMENT,
-        MONTH INT(2) NOT NULL,
-        HOUR INT(2) NOT NULL,
-        POWER_TIMEZONE VARCHAR(10) NOT NULL,
-        CONSTRAINT TB_TZ_MST_PK PRIMARY KEY(ID)
-    )
-""")
-
-my_cursor.execute("""
-    INSERT IGNORE INTO TB_TEST (ID) VALUES 
-    (1),
-    (2);
-""")
-my_cursor.execute("SELECT * FROM TB_TEST;")
-my_cursor.fetchall()
-
-=======
 
 class DBManager:
     def __init__(self, db_config):
@@ -90,4 +60,3 @@ def main():
 
 if __name__ == "__main__":
     main()
->>>>>>> test
